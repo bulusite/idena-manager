bec27437b9d4ad6627e0b61a30ab02e8e59414dd
Crackowich <5618877-crackowich@users.noreply.gitlab.com>
2020-04-25 14:48:22 +0000

    Added support for private key

--- idena-run.bat
+++ idena-run.bat
@@ -2,22 +2,31 @@
 
 Rem Change YOUR_VPS_IP with your VPS IP (with quotes)
 Rem Change YOUR_VPS_PASSWORD with your VPS PASSWORD (with quotes)
+Rem If you want to use your private key for login, specify it as VPS_PRIVATEKEY, VPS_PASSWOD will be ignored
 
 set VPS_IP="YOUR_VPS_IP"
 set VPS_PASSWORD="YOUR_VPS_PASSWORD"
+set VPS_PRIVATEKEY=""
 
 set VPS_USER="root"
 set VPS_PORT=22
 set VPS_RPC_PORT=9009
+set VPS_NODE_HOST="localhost"
 set LOCAL_RPC_PORT=9999
 set RUN_AS_USER=""
 
 Rem DO NOT EDIT ANYTHING BELOW THIS LINE!
 
 if exist "%ProgramFiles%\PuTTY\putty.exe" (
-    start "Putty" /B "%ProgramFiles%\PuTTY\putty.exe" -ssh -L %LOCAL_RPC_PORT%:localhost:%VPS_RPC_PORT% %VPS_USER%@%VPS_IP% %VPS_PORT% -pw %VPS_PASSWORD%
+	set PUTTY_LOCATION="%ProgramFiles%\PuTTY\putty.exe"
 ) else (
-    start "Putty" /B "%ProgramFiles(x86)%\PuTTY\putty.exe" -ssh -L %LOCAL_RPC_PORT%:localhost:%VPS_RPC_PORT% %VPS_USER%@%VPS_IP% %VPS_PORT% -pw %VPS_PASSWORD%
+	set PUTTY_LOCATION="%ProgramFiles(x86)%\PuTTY\putty.exe"
+)
+
+if %VPS_PRIVATEKEY% == "" (
+	start "Putty" /B %PUTTY_LOCATION% -ssh -L %LOCAL_RPC_PORT%:%VPS_NODE_HOST%:%VPS_RPC_PORT% %VPS_USER%@%VPS_IP% %VPS_PORT% -pw %VPS_PASSWORD%
+) else (
+	start "Putty" /B %PUTTY_LOCATION% -ssh -L %LOCAL_RPC_PORT%:%VPS_NODE_HOST%:%VPS_RPC_PORT% %VPS_USER%@%VPS_IP% %VPS_PORT% -i "%VPS_PRIVATEKEY%"
 )
 
 if %RUN_AS_USER% == "" (
